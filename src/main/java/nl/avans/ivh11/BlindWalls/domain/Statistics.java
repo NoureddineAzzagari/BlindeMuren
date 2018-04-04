c3f4cba97c4106bacda4b65e52fae2d2fb0bc127
vdjahoge <vdjahoge@avans.nl>
2018-04-04 11:05:18 +0200

    Added singleton to track stats. current version only tracks amount of searches

---
+++ src/main/java/nl/avans/ivh11/BlindWalls/domain/Statistics.java
@@ -0,0 +1,24 @@
+package nl.avans.ivh11.BlindWalls.domain;public class Statistics {
+
+    //create an object of Statistics. search starts at 0
+    private static Statistics instance = new Statistics();
+    private int amountSearch = 0;
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private Statistics(){}
+
+    //Get the only object available
+    public static Statistics getInstance(){
+        return instance;
+    }
+
+    public int getSearch() {
+        return amountSearch;
+    }
+
+    public void addSearch() {
+        this.amountSearch++;
+    }
+
+}
